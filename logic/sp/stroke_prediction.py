e83643c6b1f8045db99ebfecac2905123bdd3880
I543540 <julian.kominiak@sap.com>
2021-05-23 13:52:51 +0200

    move

--- logic/sp/stroke_prediction.py
+++ logic/sp/stroke_prediction.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import numpy as np
-import random as rd
+from textblob import TextBlob
+from flask import Flask, request, jsonify
 from DataProcessing import DataProcessing
 from Classificators import *
+
+app = Flask(__name__)
 
 data = pd.read_csv('dataset.csv', index_col="Unnamed: 0")
 sample = pd.Series(data={'is_female': 0, 'age': 81, 'is_urban': 1, 'is_working': 0.75,
@@ -12,3 +14,7 @@ BayesPrediction = NaiveBayes.classify(data, sample)
 KNNPrediction = KNN.clustering(sample, data, 3)
 StrokePrediction = (float(0.94) * float(BayesPrediction) + float(0.93) * float(KNNPrediction)) / 2
 print("Probability of stroke is:", round(StrokePrediction * 100, 1),"%")
+
+@app.route("/analyse/sample_data", methods=['POST'])
+def analyse_data():
+    return
